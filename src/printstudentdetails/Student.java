b7986c5bbc4aad4afb62d2383c41d994c74b84bb
sinjaskr <123108815+sinjaskr@users.noreply.github.com>
2023-01-26 12:39:07 -0500

    Update Student.java

--- src/printstudentdetails/Student.java
+++ src/printstudentdetails/Student.java
@@ -1,4 +1,6 @@
 /*
+ * Hello Jatin
+ * How are you?
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
